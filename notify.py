b02af089ccc9fc52559396852d3822813e0c4d68
ketan-16 <51126671+ketan-16@users.noreply.github.com>
2020-03-27 23:25:13 +0530

    Update notify.py
    Added API link and comments.

--- notify.py
+++ notify.py
@@ -8,7 +8,7 @@ while(True):
     quote2 = float(Nse().get_quote("YESBANK")['lastPrice'])
     print("RCOM: ",quote1,"YESBANK:", quote2)
     if quote1<10 or quote2<0.26:
-        y = requests.get('https://maker.ifttt.com/trigger/price_dropped/with/key/pl1VWQ5BWRHXona1uvQ7c9ZjuhhMX0-A-G0lXqg70gX')
+        y = requests.get('https://maker.ifttt.com/trigger/price_dropped/with/key/pl1VWQ5BWRHXona1uvQ7c9ZjuhhMX0-A-G0lXqg70gX') #This is my key! Everytime this request I send get your key from IFTTT Webhooks API Documentation.
     time.sleep(1)
     
 
