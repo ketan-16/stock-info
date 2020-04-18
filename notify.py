58d1bc49927e142c23fe6d72cb35160f84249bf0
Ketan Yadav <ktnydv@gmail.com>
2020-04-18 22:10:56 +0530

    added one more stock

--- notify.py
+++ notify.py
@@ -4,11 +4,12 @@ import time,requests
 x = requests.get('https://maker.ifttt.com/trigger/notify/with/key/pl1VWQ5BWRHXona1uvQ7c9ZjuhhMX0-A-G0lXqg70gX')
 
 while(True):
-    quote1 = float(Nse().get_quote("RCOM")['lastPrice'])
-    quote2 = float(Nse().get_quote("YESBANK")['lastPrice'])
-    print("RCOM: ",quote1,"YESBANK:", quote2)
-    if quote1<10 or quote2<0.26:
-        y = requests.get('https://maker.ifttt.com/trigger/price_dropped/with/key/pl1VWQ5BWRHXona1uvQ7c9ZjuhhMX0-A-G0lXqg70gX') #This is my key! Everytime this request I send get your key from IFTTT Webhooks API Documentation.
+    quote1 = float(Nse().get_quote("YESBANK")['lastPrice'])
+    quote2 = float(Nse().get_quote("RCOM")['lastPrice'])
+    quote3 = float(Nse().get_quote("IDEA")['lastPrice'])
+    print("YESBANK: ",quote1,"RCOM:", quote2, "IDEA: ", quote3)
+    if quote1<10 or quote2<0.26 or quote3<3:
+        y = requests.get('https://maker.ifttt.com/trigger/price_dropped/with/key/pl1VWQ5BWRHXona1uvQ7c9ZjuhhMX0-A-G0lXqg70gX')
     time.sleep(1)
     
 
